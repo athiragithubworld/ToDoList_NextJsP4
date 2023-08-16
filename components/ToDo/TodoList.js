6c048b0f2e3d8c366b55e33c579cbecb367115ae
athiragithubworld <athirahrms@gmail.com>
2023-08-16 20:18:52 +0530

    TodoList js and css - Task 10

---
+++ components/ToDo/TodoList.js
@@ -0,0 +1,75 @@
+import React, { useContext, useEffect, useState } from "react";
+import Card from "../UI/Card";
+import classes from "./TodoList.module.css";
+import { AiFillCloseCircle } from "react-icons/ai";
+import ToDoContext from "../store/ToDoContext";
+
+const TodoList = (props) => {
+  const todocntx = useContext(ToDoContext);
+
+  console.log("todo", props.todoList);
+
+  const [todoList, setTodoList] = useState(todocntx.toDoList);
+
+  useEffect(() => {
+    setTodoList(todocntx.toDoList);
+  }, [todocntx.toDoList]);
+
+  const onDeleteHandler = (item) => {
+    const delTodo = todoList.filter((todoItem) => todoItem.id !== item.id);
+    setTodoList(delTodo);
+    todocntx.removetoDoList(item);
+  };
+
+  const handleCheckboxChange = (id) => {
+    setTodoList((prevData) =>
+      prevData.map((row) =>
+        row.id === id ? { ...row, doneTask: !row.doneTask } : row
+      )
+    );
+    todocntx.todoTaskDone(id);
+  };
+
+  return (
+    <Card>
+      <div className={classes.control}>
+        <h1>Today's Todo List</h1>
+      </div>
+      <div>
+        <table className={classes.table}>
+          <tbody>
+            {todoList &&
+              todoList.map((item) => {
+                return (
+                  <tr className={classes.tr} key={item.key}>
+                    <td>
+                      <input
+                        type="checkbox"
+                        checked={item.doneTask}
+                        onChange={(e) => {
+                          e.stopPropagation();
+                          handleCheckboxChange(item.id);
+                        }}
+                      ></input>
+                    </td>
+                    <td className={classes.td}>{item.todoContent}</td>
+
+                    <td>
+                      <AiFillCloseCircle
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          onDeleteHandler(item);
+                        }}
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
+    </Card>
+  );
+};
+
+export default TodoList;
