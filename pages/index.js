ae7c1f245c8838168a348b6b87effba61128fa70
athiragithubworld <athirahrms@gmail.com>
2023-08-17 12:48:53 +0530

    index.js (pages) - Task 11 - ( GET data from Mongodb)

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,24 @@ import NewTodo from "@/components/ToDo/NewTodo";
 import TodoList from "@/components/ToDo/TodoList";
 // import TodoList from "@/components/ToDo/TodoList";
 import React, { Fragment, useState } from "react";
+import { MongoClient } from "mongodb";
 
 const index = (props) => {
-  const [todoList, setTodoList] = useState([]);
-  const addTodoListHandler = (addedTodoList) => {
-    console.log(addedTodoList);
-    console.log("props", props);
-    setTodoList((prevState) => {
-      const newTodoList = [...prevState, addedTodoList];
-      return newTodoList;
+  // const [todoList, setTodoList] = useState([]);
+
+  const addTodoListHandler = async (addedTodoList) => {
+    console.log("todolist", addedTodoList);
+
+    const response = await fetch("/api/new-todo", {
+      method: "POST",
+      body: JSON.stringify(addedTodoList),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
+
+    const data = await response.json();
+    console.log(data);
   };
 
   return (
@@ -21,10 +29,36 @@ const index = (props) => {
         <NewTodo onTodoList={addTodoListHandler} />
       </div>
       <div>
-        <TodoList todoList={todoList} />
+        <TodoList todoList={props.todoList} />
       </div>
     </Fragment>
   );
 };
+
+export async function getStaticProps() {
+  // fetch data from api
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://Athipython:twmE5fn6eWqn6gYx@cluster0.jnw4ana.mongodb.net/todoLists?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const todoCollections = db.collection("todoLists");
+  const todoLists = await todoCollections.find().toArray();
+
+  return {
+    props: {
+      todoList: todoLists.map((item) => ({
+        id: item.id,
+        todoContent: item.todoContent,
+        date: item.date,
+        doneTask: item.doneTask,
+        key: item._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
 
 export default index;
